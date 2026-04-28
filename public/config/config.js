8547f91ce2a84f3ebf2d393dba19e853090b90a5
Duguce <zhgyqc@163.com>
2026-04-28 19:32:15 +0800

    updating site on Tue Apr 28 19:32:15 CST 2026

---
+++ public/config/config.js
@@ -0,0 +1,6 @@
+// 生产环境配置 - 会被提交到 GitHub
+// zhgyqc.site 域名白名单已设置
+window.AMAP_CONFIG = {
+  key: "01c62dbb840ff3a9608824515767257a",
+  securityCode: "15ad4d83b7c5222fb9809fcb2d3b9d58"
+};
---
+++ public/config/config.local.js
@@ -0,0 +1,6 @@
+// 本地调试配置 - 不会被提交到 GitHub
+// 用于本地开发时使用不同的高德地图 Key
+window.AMAP_CONFIG = {
+  key: "f3a6274bc975a9b28934a86ca96ecbc4",
+  securityCode: "e9a6da76d6e6fde32806b857e2cf98cb"
+};
