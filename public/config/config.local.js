e285935698a374b379f25f110adcfb3123790b50
Duguce <zhgyqc@163.com>
2026-05-01 15:53:23 +0800

    updating site on Fri May  1 15:53:22 CST 2026

--- public/config/config.local.js
+++ public/config/config.local.js
@@ -2,5 +2,6 @@
 // 用于本地开发时使用不同的高德地图 Key
 window.AMAP_CONFIG = {
   key: "f3a6274bc975a9b28934a86ca96ecbc4",
-  securityJsCode: "e9a6da76d6e6fde32806b857e2cf98cb"
+  securityJsCode: "e9a6da76d6e6fde32806b857e2cf98cb",
+  webKey: ""
 };
--- static/config/config.js
+++ static/config/config.js
@@ -2,5 +2,6 @@
 // zhgyqc.site 域名白名单已设置
 window.AMAP_CONFIG = {
   key: "01c62dbb840ff3a9608824515767257a",
-  securityJsCode: "15ad4d83b7c5222fb9809fcb2d3b9d58"
+  securityJsCode: "15ad4d83b7c5222fb9809fcb2d3b9d58",
+  webKey: ""
 };
