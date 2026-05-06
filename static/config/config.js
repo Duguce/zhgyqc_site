0fccae59423206f7853d8333163bc1d3a9ba3d51
Duguce <zhgyqc@163.com>
2026-05-06 21:56:12 +0800

    updating site on Wed May  6 21:56:12 CST 2026

--- static/config/config.js
+++ static/config/config.js
@@ -1,5 +1,5 @@
-// 生产环境配置 - 会被提交到 GitHub
-// zhgyqc.site 域名白名单已设置
+// Production environment configuration - will be committed to GitHub
+// Domain whitelist for zhgyqc.site has been set
 window.AMAP_CONFIG = {
   key: "01c62dbb840ff3a9608824515767257a",
   securityJsCode: "15ad4d83b7c5222fb9809fcb2d3b9d58",
